299b53e68609e6c97e4bff75269688677a15fe97
Chris Dexter <Chris.Dexter@synx.com>
2020-11-08 09:12:29 +0000

    Added DiscountRepository

---
+++ src/CheckoutSimulator.Persistence/DiscountRepository.cs
@@ -0,0 +1,32 @@
+﻿// Checkout Simulator by Chris Dexter, file="DiscountRepository.cs"
+
+namespace CheckoutSimulator.Persistence
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using CheckoutSimulator.Domain.Offers;
+    using CheckoutSimulator.Domain.Repositories;
+
+    /// <summary>
+    /// Defines the <see cref="DiscountRepository"/>.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class DiscountRepository : IDiscountRepository
+    {
+        /// <summary>
+        /// The GetDiscountsAsync.
+        /// </summary>
+        /// <returns>The <see cref="Task{IEnumerable{IDiscount}}"/>.</returns>
+        public Task<IEnumerable<IDiscount>> GetDiscountsAsync()
+        {
+            var discounts = new List<IDiscount>()
+            {
+                new ItemDiscount("Buy One - Get One Free")
+            };
+
+            return Task.FromResult(discounts.AsEnumerable());
+        }
+    }
+}
